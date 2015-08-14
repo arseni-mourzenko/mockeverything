caa16c7db1111bf8e1ddc1d0a768b6c9f8db46ca
Arseni Mourzenko <arseni.mourzenko@gmail.com>
2015-08-15 00:43:30 +0200

    Tested deferred loading of the assembly.

--- MockEverything/Tests/Inspection/MonoAssemblyTests.cs
+++ MockEverything/Tests/Inspection/MonoAssemblyTests.cs
@@ -3,12 +3,29 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using MockEverything.Inspection.MonoCecil;
     using System;
+    using System.IO;
     using System.Linq;
     using Reflection = System.Reflection;
 
     [TestClass]
     public class MonoAssemblyTests
     {
+        [TestMethod]
+        public void ConstructorWithWrongNameLazy()
+        {
+            // The assembly shouldn't be loaded when a constructor is called, but only when we actually need it. Thus, calling a constructor with a name shouldn't be a problem.
+            var assembly = new Assembly(@"not a real path");
+            Assert.IsNotNull(assembly);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ConstructorWithWrongName()
+        {
+            var assembly = new Assembly(@"not a real path");
+            assembly.FindTypes().ToList();
+        }
+
         [TestMethod]
         public void TestFindTypes()
         {
