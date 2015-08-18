24c13ee486de64a8b64d95c7b7e66f1a22774977
Arseni Mourzenko <arseni.mourzenko@gmail.com>
2015-08-18 15:11:26 +0200

    Moved assembly browser related classes to a dedicated directory.

---
+++ MockEverything/Source/Engine/Browsers/Type level/TypeBrowser.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="AssemblyBrowser.cs">
+//      Copyright (c) Arseni Mourzenko 2015. The code is distributed under the MIT License.
+// </copyright>
+// <author id="5c2316d3-622a-4a8d-816d-5054a48f415f">Arseni Mourzenko</author>
+
+namespace MockEverything.Engine.Browsers
+{
+    public class TypeBrowser
+    {
+    }
+}
