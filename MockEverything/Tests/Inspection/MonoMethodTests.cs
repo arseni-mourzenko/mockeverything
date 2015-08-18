d92216a23e2d74ae3b79a9bdc6c32de72d6f7d2b
Arseni Mourzenko <arseni.mourzenko@gmail.com>
2015-08-18 20:41:44 +0200

    Added tests which check whether the properties of method class work as expected.

---
+++ MockEverything/Tests/Inspection/MonoMethodTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace MockEverythingTests.Inspection
+{
+    using System.Linq;
+    using Demo;
+    using MockEverything.Inspection.MonoCecil;
+
+    [TestClass]
+    public class MonoMethodTests
+    {
+        [TestMethod]
+        public void TestGetName()
+        {
+            var actual = this.SampleAssembly
+                .FindTypes()
+                .Single(t => t.Name == "SimpleClass")
+                .FindMethods()
+                .Single(m => m.Name == "SimpleMethod")
+                .Name;
+
+            var expected = "SimpleMethod";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetReturnTypeVoid()
+        {
+            var actual = this.SampleAssembly
+                .FindTypes()
+                .Single(t => t.Name == "SimpleClass")
+                .FindMethods()
+                .Single(m => m.Name == "SimpleMethod")
+                .ReturnType
+                .FullName;
+
+            var expected = "System.Void";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetReturnTypeCustom()
+        {
+            var actual = this.SampleAssembly
+                .FindTypes()
+                .Single(t => t.Name == "SimpleClass")
+                .FindMethods()
+                .Single(m => m.Name == "get_SimpleProperty")
+                .ReturnType
+                .FullName;
+
+            var expected = "System.String";
+            Assert.AreEqual(expected, actual);
+        }
+
+        private Assembly SampleAssembly
+        {
+            get
+            {
+                return new Assembly(this.SampleAssemblyPath);
+            }
+        }
+
+        private string SampleAssemblyPath
+        {
+            get
+            {
+                var codeBase = typeof(SimpleClass).Assembly.CodeBase;
+                return System.Uri.UnescapeDataString(new System.UriBuilder(codeBase).Path);
+            }
+        }
+    }
+}
