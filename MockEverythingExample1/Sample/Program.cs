f296d846292e5ca35324d5752668c13f7d03fa37
Arseni Mourzenko <arseni.mourzenko@gmail.com>
2015-09-11 22:44:10 +0200

    Added a console sample.

---
+++ MockEverythingExample1/Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MockEverythingExample1.Sample
+{
+    using Exchanger;
+    using Library;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            WebClientExchanger.PersonName = "Jeff";
+            var actual = new Demo().FindName();
+        }
+    }
+}
