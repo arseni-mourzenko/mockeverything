8495fbfe510812194cd8835d91dde6a079774a86
Arseni Mourzenko <arseni.mourzenko@gmail.com>
2015-09-17 00:18:03 +0200

    Added NUnit example—the one which doesn't work.

---
+++ MockEverythingExample1/TestsWithNUnit/SampleTests.cs
@@ -0,0 +1,19 @@
+﻿namespace MockEverythingExample1.TestsWithNUnit
+{
+    using Exchanger;
+    using Library;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SampleTests
+    {
+        [Test]
+        public void TestFindName()
+        {
+            WebClientExchanger.PersonName = "Jeff";
+            var actual = new Demo().FindName();
+            var expected = "Jeff";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
